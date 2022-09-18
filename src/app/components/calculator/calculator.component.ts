5cdd6d1dca5200bd6ac6bd9e6e1d8f005647c861
Borislav <borislav2003@gmail.com>
2022-09-18 19:55:56 +0500

    Created numberHelper

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  input: string = '';
+  buttons: string[] = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '<-', '0', '=', '\\'];
 
   constructor() { }
 
+  onButtonClick(value: string): void {
+    this.input += value;
+  }
+
+  getButtonClass(value: string) {
+    if(this.isNumeric(value)) {
+      return 'operand';
+    }
+
+    if(value === '<-') {
+      return 'backspace';
+    }
+
+    return 'operator';
+  }
+
+  private isNumeric(str: string) {
+    return !isNaN(parseFloat(str))
+  }
+
   ngOnInit(): void {
+
   }
 
 }
